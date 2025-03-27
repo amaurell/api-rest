49b8467fdd634ad5b228684d49bff07fda917da4
Andre Maurell <amaurell@gmail.com>
2025-03-27 09:24:54 -0300

    Inserindo rota get

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,11 @@
 import express from "express";
 
+const PORT = 3333;
 
+const app = express();
+
+app.get("/", (request, response) => {
+  response.send("Hello World!");
+});
+
+app.listen(PORT, () => console.log(`Server is running at ${PORT}`));
