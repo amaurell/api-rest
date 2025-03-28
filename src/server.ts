035615fc47f35fbef7a60c9f561740e3fabd1462
Andre Maurell <amaurell@gmail.com>
2025-03-28 11:32:54 -0300

    Como tratar erros do servidor e do cliente

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
 import express, { NextFunction, Request, Response } from "express";
-
 import { routes } from "./routes";
+import { AppError } from "./utils/AppError";
 
 const PORT = 3333;
 
@@ -9,7 +9,12 @@ app.use(express.json());
 
 app.use(routes);
 
+
+
 app.use((error: any, request: Request, response: Response, _: NextFunction) => {
+  if(error instanceof AppError){
+    return response.status(error.statusCode).json({message: error.message})
+  }
   response.status(500).json({message:error.message});
 });
 
